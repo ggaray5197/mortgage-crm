f0d183d8b76f69844d799e3548cdc016e5a36b7c
ggaray5197 <ggaray5197@gmail.com>
2026-02-08 20:48:13 -0500

    Remove standalone output mode

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs'],
   },
-  // Force dynamic rendering for all pages
-  output: 'standalone',
 }
 
 module.exports = nextConfig
